e0f4d9953e77008a736cf002d54acdab41b152e1
ktogias <ktogias@cti.gr>
2022-05-11 16:05:33 +0300

    Fix /tmp/kafka permissions when exists

--- target/opt/scripts/entrypoint.sh
+++ target/opt/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
+[ -d "/tmp/kafka-logs" ] && chown -R kafka:root /tmp/kafka-logs
 rm /tmp/kafka-logs/.lock | true
-/opt/kafka/bin/kafka-server-start.sh config/server.properties --override listener.security.protocol.map=$KAFKA_LISTENER_SECURITY_PROTOCOL_MAP --override listeners=$KAFKA_LISTENERS --override advertised.listeners=$KAFKA_ADVERTISED_LISTENERS --override zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT &
-/opt/scripts/create-topics.sh
+runuser kafka -c '/opt/kafka/bin/kafka-server-start.sh config/server.properties --override listener.security.protocol.map=$KAFKA_LISTENER_SECURITY_PROTOCOL_MAP --override listeners=$KAFKA_LISTENERS --override advertised.listeners=$KAFKA_ADVERTISED_LISTENERS --override zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT &
+/opt/scripts/create-topics.sh'
 tail -f /dev/null
